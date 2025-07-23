ef79cb6577face0f8100ec9fe6416777714e3d9e
unknown <malailiyati107@gmail.com>
2025-07-23 10:08:10 +0700

    fix resolve

--- lampu/lampu.js
+++ lampu/lampu.js
@@ -4,27 +4,29 @@ const green = document.querySelector(".green"); //mengambil elemen class green
 
 async function trafficLight() {
   while (true) {
-    //loop terus menerus
     await new Promise((resolve) => {
       setTimeout(() => {
         red.style.opacity = 1; //lampu merah nyala
         yellow.style.opacity = 0.5;
         green.style.opacity = 0.5;
-      }, 0); //delay awal
+        resolve();
+      }, 3000); //delay awal
     });
     await new Promise((resolve) => {
       setTimeout(() => {
         red.style.opacity = 0.5;
         yellow.style.opacity = 1; //lampu kuning nyala
         green.style.opacity = 0.5;
-      }, 3000); //delay
+        resolve();
+      }, 2000); //delay
     });
     await new Promise((resolve) => {
       setTimeout(() => {
         red.style.opacity = 0.5;
         yellow.style.opacity = 0.5;
         green.style.opacity = 1; //lampu hijau nyala
-      }, 2000); //delay
+        resolve();
+      }, 3000); //delay
     });
   }
 }
